5343e12eef242082b640889b458e56f4f6cdf12e
05126619z <05126619z@gmail.com>
2025-05-30 15:35:32 +0300

    Stubs for downloader

---
+++ ScavLib/Modules/Downloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ScavLib.Modules
+{
+    /// <summary>
+    /// Module which is used to download content then return it
+    /// Used internaly, to use refer to ScavLib.Commands
+    /// TODO!
+    /// </summary>
+    internal class Downloader
+    {
+
+    }
+}
---
+++ ScavLib/Objects/DownloaderStuff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScavLib.Objects
+{
+    /// <summary>
+    /// TODO!
+    /// </summary>
+    internal static class DownloaderStuff
+    {
+
+    }
+}
